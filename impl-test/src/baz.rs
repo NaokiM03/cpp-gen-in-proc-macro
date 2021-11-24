a9711c88d057ecef0a30321d43462da3d8629851
NaokiM03 <naokim03.lib+dev@gmail.com>
2021-11-24 13:42:12 +0900

    マクロをきちんと作ればここでも書ける

--- impl-test/src/baz.rs
+++ impl-test/src/baz.rs
@@ -4,3 +4,13 @@ use crate::foo;
 fn test_err() {
     assert_ne!(foo(), 41);
 }
+
+// use crate::bar;
+
+// #[bar]
+// fn foo() -> i32 {}
+
+// #[test]
+// fn test_ok() {
+//     assert_eq!(foo(), 42);
+// }
